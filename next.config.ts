cc76d3e7cbe21aef42ceb10e0b08f2916ce6ce31
Aditya Rao <adityarao2005@gmail.com>
2025-12-16 23:14:18 -0500

    updated it to work but unforunately no tailwind usage

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  sassOptions: {
+    implementation: 'sass',
+    silenceDeprecations: ['legacy-js-api'],
+    api: 'modern-compiler',
+  },
   /* config options here */
+  turbopack: {
+    resolveAlias: {
+      "~*": "*",
+    },
+  },
 };
 
 export default nextConfig;
